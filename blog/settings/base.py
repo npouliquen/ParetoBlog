a92407f40f1cf0637c3428df4efb21997395e00c
npouliquen <nick@teampareto.com>
2016-08-02 14:14:02 -0500

    added amazon s3 support. Reformatted edit and new post pages.

--- blog/settings/base.py
+++ blog/settings/base.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
     'markdownx',
     'pagedown',
     'markdown_deux',
+    'storages',
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -127,6 +128,20 @@ USE_L10N = True
 
 USE_TZ = True
 
+# Amazon S3 Settings
+AWS_STORAGE_BUCKET_NAME = 'paretoengineeringblog'
+AWS_ACCESS_KEY_ID = 'AKIAIKDUCS7K7W7KYY7Q'
+AWS_SECRET_ACCESS_KEY = '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'
+
+# Tell django-storages that when coming up with the URL for an item in S3 storage, keep
+# it simple - just use this domain plus the path. (If this isn't set, things get complicated).
+# This controls how the `static` template tag from `staticfiles` gets expanded, if you're using it.
+# We also use it in the next setting.
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+
+MEDIAFILES_LOCATION = 'media'
+MEDIA_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, MEDIAFILES_LOCATION)
+DEFAULT_FILE_STORAGE = 'blog.apps.blogpost.s3utils.MediaS3BotoStorage'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
