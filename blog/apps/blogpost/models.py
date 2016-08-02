dc51ef31e18daa67e708dbdb63438f977626363c
npouliquen <nick@teampareto.com>
2016-08-02 15:11:32 -0500

    made function that will find url of image and make it top image in card.

--- blog/apps/blogpost/models.py
+++ blog/apps/blogpost/models.py
@@ -12,6 +12,16 @@ class Post(models.Model):
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
 
+    def get_image(self):
+        index = self.text.find("![]")
+        if (index > -1):
+            start_of_url = index + 4
+            end_of_url = self.text.find(')', start_of_url)
+            url = self.text[start_of_url:end_of_url]
+            return url
+        else:
+            return "https://s3.amazonaws.com/paretoengineeringblog/ParetoLogo1.png"
+
     def publish(self):
         self.published_date = timezone.now()
         self.save()
